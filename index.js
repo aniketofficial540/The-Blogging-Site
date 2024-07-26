5ae669a33b4cdbf136b159c01c278d2dfd5a50fa
aniketofficial540 <160538419+aniketofficial540@users.noreply.github.com>
2024-07-26 15:02:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(("public")));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+    global.checkBlog = null;
+    global.inUsername = null;
+    res.render("home.ejs", {
+        username: inUsername,
+        checkBlog : checkBlog
+    });
+});
+
+app.get("/signup", (req,res) => {
+    res.render("signup.ejs");
+})
+
+app.post("/signup", (req, res) => {
+    inUsername = req.body.username;
+    global.inPassword = req.body.password;
+    res.render("login.ejs");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.post("/login", (req, res) => {
+    global.inUsername_check = req.body.username;
+    global.inPassword_check = req.body.password;
+
+    
+    if (inUsername_check === inUsername && inPassword_check === inPassword) {
+        res.render("home.ejs", {
+            username: inUsername_check
+        });
+    }
+    else{
+        res.redirect("login");
+    }
+})
+
+app.get("/blog", (req, res) => {
+    res.render("blog.ejs");
+});
+
+app.post("/blog", (req, res) => {
+    global.topic = req.body.topic;
+    global.blogText = req.body.blogText;
+    checkBlog = true;
+
+    res.render("home.ejs", {
+        username: inUsername,
+        topic : topic,
+        blogText : blogText,
+        checkBlog : checkBlog
+    })
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
